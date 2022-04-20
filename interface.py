adf5d76be2174c9f857a6bad05ef6deff317792a
GENZLINGER <bgenzlinger@tea-bgenzlinger.isf.edu.hk>
2022-04-20 11:44:38 +0800

    Filled in interface.py

--- interface.py
+++ interface.py
@@ -1,10 +1,27 @@
 from simple_term_menu import TerminalMenu
-#import animal
-#set up animal with interactions
-choice = "Play"
+from pet import Pet  #import Pet class
+my_pet = Pet() #create the pet
 
-while(choice != "Quit"):
-    options = ["Entry 1", "Entry 2", "Entry 3", "Quit"] #set the menu choices
+choice_num = 0
+
+while(choice_num != 3):
+    options = ["Name your pet", "Entry 2", "Entry 3", "Quit"] #set the menu choices
     terminal_menu = TerminalMenu(options) #create the menu
     choice_num = terminal_menu.show() #user selects
-    choice = options[choice_num] #save the user's choice
+
+    if choice_num == 0:
+        print("-"*30) #print a line
+        print("What would you like to name your pet?") #print the prompt
+        name = input(">>> ") #get the user input
+        my_pet.set_name(name) #set the pet's name
+        print("-"*30) #print a line
+
+    elif choice_num == 1:
+        #Replace this with your own code
+        print("You chose", options[choice_num])
+
+    elif choice_num == 2:
+        #Replace this with your own code
+        print("You chose", options[choice_num])
+
+print("Goodbye")
